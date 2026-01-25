08a22535a99c452ddc23f83cd2a38b134801cfc3
Robertas-Cyberattack <robertas.sladkevicius@gmail.com>
2026-01-25 18:40:37 +0000

    tickets.html renamed home.html

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 
 # -------------------- HOME --------------------
 @app.route("/")
-def tickets():
+def home():
     # 6 sample projects
     projects = [
         {"title": "Residential Apartment Design", "description": "Complete CAD drafting for a modern apartment complex in London, following British Building Codes.", "image": "project1.jpg"},
